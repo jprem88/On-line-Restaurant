b22227d21a8a6180643fa6ff49cfaa0271deb40f
jprem88 <88prem@gmail.com>
2026-02-14 11:33:23 +0530

    new controller added

---
+++ Mango.Web/Controllers/HomeController2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class HomeController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
